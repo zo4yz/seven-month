e4795428a3a0a3a50ad59fd3929d9f46dd1840a1
zo4yz <140940249+zo4yz@users.noreply.github.com>
2024-08-27 13:51:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -56,13 +56,10 @@ document.querySelectorAll('.service-card').forEach(card => {
         card.style.backgroundColor = 'white'; // Reset to white when not hovering
     });
 });
-
-const slider = document.querySelector('.slider');
-
-function activate(e) {
+ const slider = document.querySelector('.slider');
+ function activate(e) {
     const items = document.querySelectorAll('.item');
     e.target.matches('.next') && slider.append(items[0])
     e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
 }
-
 document.addEventListener('click', activate, false);
